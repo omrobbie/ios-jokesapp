9095113697ba85a222f149b275a3fd20bb1344b1
omrobbie <putu.roby@yahoo.com>
2020-03-10 14:19:49 +0800

    Initial Commit

---
+++ JokesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JokesApp
+//
+//  Created by omrobbie on 10/03/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
